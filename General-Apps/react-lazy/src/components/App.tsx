9920cc0da9ada21f0cd9587d313658a3bb8b0dce
Shubham Jangle <jangleshubham13@gmail.com>
2024-12-06 12:44:48 +0530

    refactor

--- General-Apps/react-lazy/src/components/App.tsx
+++ General-Apps/react-lazy/src/components/App.tsx
@@ -1,24 +1,35 @@
-import { lazy, Suspense } from "react";
+import React, { lazy } from "react";
+
 import LightComponent from "./LightComponent";
+import LazyLoadInViewWrapper from "./LazyLoadInViewWrapper";
+
 // import HeavyComponent from "./HeavyComponent";
 const HeavyComponent = lazy(() => import("./HeavyComponent"));
 
-const App = () => {
+// Main Dashboard Component
+const App2 = () => {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">React Lazy Loading</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <LightComponent /> {/* 0.4s */}
-        <Suspense
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <LightComponent />
+        <LightComponent />
+        <LightComponent />
+        <LightComponent />
+        <LightComponent />
+        <LazyLoadInViewWrapper
           fallback={
-            <div className="h-64 bg-gray-200 animate-pulse p-2">Loading...</div>
+            <div className="h-64 bg-gray-200 animate-pulse p-2">
+              Loading User Stats...
+            </div>
           }
         >
-          <HeavyComponent /> {/* 4s */}
-        </Suspense>
+          <HeavyComponent />
+        </LazyLoadInViewWrapper>
+        <LightComponent />
       </div>
     </div>
   );
 };
 
-export default App;
+export default App2;
