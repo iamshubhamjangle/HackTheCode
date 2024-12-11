54d3f3673d6419e4af6017c6206dabeb6e468530
Shubham Jangle <jangleshubham13@gmail.com>
2024-12-11 15:48:22 +0530

    feat: progressive images

--- General-Apps/react-lazy/src/components/Gallery.tsx
+++ General-Apps/react-lazy/src/components/Gallery.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const ImageGallery = () => {
   const imageCount = 20;
   const images = Array.from({ length: imageCount }, (_, index) => {
@@ -17,15 +19,64 @@ const ImageGallery = () => {
             key={index}
             className="relative overflow-hidden rounded-lg bg-gray-200"
           >
-            <img
+            {/* <img
               src={src.full}
               alt={`Gallery image ${index + 1}`}
               loading="lazy"
               className="w-full h-96 object-cover"
+            /> */}
+            <ProgressiveImage
+              fullSrc={src.full}
+              thumbnailSrc={src.thumbnail}
+              alt={`Gallery image ${index + 1}`}
             />
           </div>
         ))}
       </div>
+    </div>
+  );
+};
+
+interface ProgressiveImageProps {
+  fullSrc: string;
+  thumbnailSrc: string;
+  alt: string;
+}
+
+const ProgressiveImage: React.FC<ProgressiveImageProps> = ({
+  fullSrc,
+  thumbnailSrc,
+  alt,
+}) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  return (
+    <div className="relative w-full pb-[150%]">
+      {/* Thumbnail (always visible initially) */}
+      <img
+        src={thumbnailSrc}
+        alt={`${alt} thumbnail`}
+        loading="lazy"
+        className={`
+            absolute top-0 left-0 w-full h-full 
+            object-cover 
+            transition-opacity duration-300
+            ${imageLoaded ? "opacity-0" : "opacity-100"}
+          `}
+      />
+      {/* Full resolution image */}
+      <img
+        src={fullSrc}
+        alt={alt}
+        loading="lazy"
+        onLoad={() => setImageLoaded(true)}
+        className={`
+            absolute top-0 left-0 w-full h-full 
+            object-cover 
+            transition-opacity duration-300
+            ${imageLoaded ? "opacity-100" : "opacity-0"}
+          `}
+      />
     </div>
   );
 };
